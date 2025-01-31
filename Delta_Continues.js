dc82b7a13a6316a6a45cec3b657380a9639c7621
sanjana292006 <sanjanajayam45@gmail.com>
2025-01-31 11:27:26 +0530

    Add files via upload

---
+++ Delta_Continues.js
@@ -0,0 +1,17 @@
+alert("Welcome to Delta");
+function jv_don(){
+    var first_name=document.getElementById("fname").value;
+    var last_name=document.getElementById("lname").value;
+    var phone_num=document.getElementById("pno").value;
+    var email_id=document.getElementById("ebox").value;
+    var data_id=("Name : "+first_name+" "+last_name+" "+" Phone No. : "+phone_num+" "+" Email ID : "+email_id);
+    alert("Name : "+first_name+" "+last_name)
+    alert("Phone No. : "+phone_num)
+    alert("Email ID : "+email_id)
+    alert(data_id)
+    alert("Copyrighted by Wildfire Puneetha©️")
+    
+}
+function intro() {
+    window.open("");
+}
